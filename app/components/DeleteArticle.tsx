cbdb29ebf111e98bdce5b6052a65063f2d6d5fb4
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-01-14 20:49:41 +0700

    fix character content

--- app/components/DeleteArticle.tsx
+++ app/components/DeleteArticle.tsx
@@ -44,7 +44,7 @@ const DeleteArticle = ({articleId}: {articleId: number}) => {
                             </svg>
                             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
                             <button onClick={() => deleteHandle(articleId)} type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
-                                Yes, I'm sure
+                                Yes, I am sure
                             </button>
                             <button onClick={handleChange} type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">No, cancel</button>
                         </div>
